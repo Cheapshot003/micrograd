4a8ee42781f68a28b7cc53301cacdaa097864f80
cheapshot003 <dercheapi@protonmail.com>
2024-01-11 22:19:08 +0100

    pow

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Value:
-    def __init__(self, data) -> None:
+    def __init__(self, data, _children) -> None:
         self.data = data
         self.grad = 0
     
@@ -16,16 +16,4 @@ class Value:
         out = Value(self.data * other.data)
 
         return out
-
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "This operation only accepts int/float as second operator"
-
-        out = Value(self.data ** other)
-        return out
-
-#testing stuff
-if __name__ == "__main__":
-    a = Value(5)
-    b = Value(6)
-    c = a ** b.data
-    print(c.data)
+    
