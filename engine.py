f51080f7cf36ffcf17bb513d8979383c6135fb33
cheapshot003 <dercheapi@protonmail.com>
2024-01-17 19:07:54 +0100

    some stuff

--- engine.py
+++ engine.py
@@ -1,27 +1,37 @@
 import numpy as np
 
 class Value:
-    def __init__(self, data) -> None:
+    def __init__(self, data, _children=[], _op="") -> None:
         self.data = data
         self.grad = 0
-    
+
+        self._backward = lambda: None
+
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, [self, other], "+")
 
+        def _backward():
+            return ()
+        self._backward =
         return out
-    
+
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, [self, other], _op="*")
 
         return out
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "This operation only accepts int/float as second operator"
 
-        out = Value(self.data ** other)
+        out = Value(self.data ** other, [self, other], "**")
         return out
+    def backward(self):
+        self.grad = 1
+
+        return
+
 
 #testing stuff
 if __name__ == "__main__":
